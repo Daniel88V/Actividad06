4092f5292f5488a12c14df96ae13081370e25f34
Daniel88V <daniel12velo@gmail.com>
2025-07-15 00:18:39 -0600

    Tienda de ropa

--- Tienda_ropa.py
+++ Tienda_ropa.py
@@ -4,9 +4,22 @@ print("Cuantos productos desea ingresar?")
 cantidad = int(input())
 for i in range(cantidad):
     print(f"Producto {i+1}")
-    codigo = input(f"Ingrese el código del producto: ")
+    while True:
+        codigo = input(f"Ingrese el código del producto: ")
+        if codigo in productos:
+            print(f"Error, codigo existente. Pruebe de nuevo")
+        else:
+            break
     nombre = input(f"Ingrese el nombre del producto: ")
     categoria = input(f"Ingrese la categoria del producto: ")
     talla =input(f"Ingrese la talla del producto: ")
+
     precio = float(input(f"Ingrese el precio del producto: "))
     cantidad = int(input(f"Ingrese la cantidad del producto: "))
+    productos[codigo] = {
+        "nombre": nombre,
+        "categoria": categoria,
+        "talla": talla,
+        "precio": precio,
+        "cantidad": cantidad,
+    }
